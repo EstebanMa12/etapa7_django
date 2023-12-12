a660d0f3f55eaca05c6bb8a485e541290bc3feb9
EstebanMa12 <desteban.maya@udea.edu.co>
2023-12-12 17:10:13 -0500

    Update user admin fields and fieldsets.

--- user/admin.py
+++ user/admin.py
@@ -38,38 +38,43 @@ class TeamAdmin(admin.ModelAdmin):
 
 #Models for users
 from .models import CustomUser
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import (
+    AdminPasswordChangeForm,
+    UserChangeForm,
+    UserCreationForm,
+)
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id','email', 'team', 'is_superuser', 'is_staff', 'is_active')
-    list_filter = ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
-    list_display_links = ('id', 'email')
-    search_fields = ('email', 'team__team_name')
-    ordering = ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
+    list_display = ('id','username', 'team', 'is_superuser', 'is_staff', 'is_active')
+    list_filter = ('username', 'team', 'is_superuser', 'is_staff', 'is_active')
+    list_display_links = ('id', 'username')
+    search_fields = ('username', 'team__team_name')
+    ordering = ('username', 'team', 'is_superuser', 'is_staff', 'is_active')
     list_per_page = 10
     fieldsets = (
         (None, {
-            'fields': ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
+            'fields': ('username','password','team', 'is_superuser', 'is_staff', 'is_active')
         }),
     )
+    change_password_form = AdminPasswordChangeForm
     
 
-class UserAdmin(BaseUserAdmin):
-    list_display = ('email',
-                    'team',
-                    'is_superuser',
-                    'is_staff',
-                    'is_active')
+# class UserAdmin(BaseUserAdmin):
+#     list_display = ('username',
+#                     'team',
+#                     'is_superuser',
+#                     'is_staff',
+#                     'is_active')
     
-    def get_fieldsets(self, request, obj: None) :
-        fieldsets = super().get_fieldsets(request, obj)
-        if obj is not None:
-            fieldsets = (
-                (None, {'fields': ('username','password')}),
-                
-            )
-        return fieldsets
-    
-    
-admin.site.unregister(CustomUser)
-admin.site.register(CustomUser, UserAdmin)
+#     def get_fieldsets(self, request, obj: None) :
+#         fieldsets = super().get_fieldsets(request, obj)
+#         if obj is not None:
+#             fieldsets = (
+#                 (None, {'fields': ('username',
+#                                 'password',
+#                                 'team',
+#                                 'is_superuser',
+#                                 'is_staff',
+#                                 'is_active')}),
+#             )
+#         return fieldsets
