d7bef2a2602f026aa5a8144921a9755f0b3f7aad
EstebanMa12 <desteban.maya@udea.edu.co>
2023-12-12 17:46:43 -0500

    Update UserAdmin class to remove unnecessary code.

--- user/admin.py
+++ user/admin.py
@@ -64,23 +64,3 @@ class CustomUserAdmin(UserAdmin):
             }),
     )
 admin.site.register(CustomUser, CustomUserAdmin)
-
-# class UserAdmin(BaseUserAdmin):
-#     list_display = ('username',
-#                     'team',
-#                     'is_superuser',
-#                     'is_staff',
-#                     'is_active')
-    
-#     def get_fieldsets(self, request, obj: None) :
-#         fieldsets = super().get_fieldsets(request, obj)
-#         if obj is not None:
-#             fieldsets = (
-#                 (None, {'fields': ('username',
-#                                 'password',
-#                                 'team',
-#                                 'is_superuser',
-#                                 'is_staff',
-#                                 'is_active')}),
-#             )
-#         return fieldsets
