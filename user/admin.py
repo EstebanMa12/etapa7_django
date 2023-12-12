5c4e55ff5f7915aa7ecf96c769ecb8e674efff8d
EstebanMa12 <desteban.maya@udea.edu.co>
2023-12-12 14:07:09 -0500

    Update Pipfile.lock, settings.py, admin.py, migrations/0001_initial.py, and models.py

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+#Models for posts
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'permission', 'is_deleted')
+    list_filter = ('author', 'permission', 'is_deleted')
+    search_fields = ('title', 'author__username', 'permission')
+    ordering = ('title', 'author', 'permission', 'is_deleted')
+    list_editable = ('permission', 'is_deleted')
+    list_per_page = 10
+    fieldsets = (
+        ('Post', {
+            'fields': ('title', 'content', 'author', 'permission')
+        }),
+    )
+    readonly_fields = ('author',)
+
+
+#Models for teams
+from .models import Team
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('team_name',)
+    list_filter = ('team_name',)
+    search_fields = ('team_name',)
+    ordering = ('team_name',)
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('team_name',)
+        }),
+    )
+    
+    
+
+#Models for users
+from .models import CustomUser
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id','email', 'team', 'is_superuser', 'is_staff', 'is_active')
+    list_filter = ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
+    list_display_links = ('id', 'email')
+    search_fields = ('email', 'team__team_name')
+    ordering = ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
+    list_per_page = 10
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
+        }),
+    )
+    # readonly_fields = ('email', 'team', 'is_superuser', 'is_staff', 'is_active')
--- user/models.py
+++ user/models.py
@@ -1,12 +1,53 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# A default model with the created_at, modified_at, is_deleted field
+class DefaultModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+        
+class Team(DefaultModel, models.Model):
+    # Teams model
+    team_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.team_name
+
 class CustomUser(AbstractUser):
     # Use email as the username field
     username = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, blank=True, default=None, null=True)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['password']
 
     def __str__(self):
         return self.email
+    
+
+# a model for Posts
+class Post(DefaultModel, models.Model):
+    # Post model
+    PUBLIC = 'public'
+    AUTHENTICATED = 'authenticated'
+    TEAM = 'team'
+    AUTHOR = 'author'
+    
+    PERMISSIONS = (
+        (PUBLIC, 'Public'),
+        (AUTHENTICATED, 'Authenticated'),
+        (TEAM, 'Team'),
+        (AUTHOR, 'Author'),
+    )
+    
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    permission = models.CharField(max_length=20, choices=PERMISSIONS, default=PUBLIC)
+    def __str__(self):
+        return self.title
+
