9b5b555514032b82c73380205bf914b51a687b4e
EstebanMa12 <desteban.maya@udea.edu.co>
2023-12-13 17:54:27 -0500

    Change view class to support both GET and POST requests.

--- posts/views.py
+++ posts/views.py
@@ -10,17 +10,31 @@ from django.core.exceptions import ValidationError
 from .permissions import UserHasEditPermission, UserHasReadPermission
 
 
-# View for create POST
-class PostCreateView(generics.CreateAPIView):
+# View for create POST and List
+class PostCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    
+    def get_permissions(self):
+        permissions = []
+        if self.request.method == 'POST':
+            permissions.append(IsAuthenticated)
+            # self.permission_classes = [IsAuthenticated,]
+        else:
+            permissions.append(UserHasReadPermission)
+            # self.permission_classes = [UserHasReadPermission,]
+        return [p() for p in permissions]      
     
     def perform_create(self, serializer):
         try:
             serializer.save(author=self.request.user)
         except ValidationError as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+    def get(self, request, *args, **kwargs):        
+        posts = self.queryset.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
 # View for edit POST
 class PostEditView(generics.UpdateAPIView):
