b41fb2d6576d37a1c0e60102a9bddf47df7d7afe
EstebanMa12 <desteban.maya@udea.edu.co>
2023-12-13 16:40:47 -0500

    Migration of permissions

---
+++ posts/permissions.py
@@ -0,0 +1,29 @@
+from rest_framework.permissions import IsAuthenticated, BasePermission
+from .models import Post
+class AdminHasEditPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Comprueba cuales son los permisos de edicion del post
+        if obj.edit_permission == Post.PUBLIC:
+            return True
+        elif obj.edit_permission == Post.AUTHENTICATED:
+            return request.user.is_authenticated
+        elif obj.edit_permission == Post.TEAM:
+            return request.user.team == obj.author.team
+        elif obj.edit_permission == Post.AUTHOR:
+            return request.user == obj.author
+        else:
+            return False
+        
+class AdminHasReadPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Comprueba cuales son los permisos de lectura del post
+        if obj.read_permission == Post.PUBLIC:
+            return True
+        elif obj.read_permission == Post.AUTHENTICATED:
+            return request.user.is_authenticated
+        elif obj.read_permission == Post.TEAM:
+            return request.user.team == obj.author.team
+        elif obj.read_permission == Post.AUTHOR:
+            return request.user == obj.author
+        else:
+            return False
--- posts/views.py
+++ posts/views.py
@@ -1,42 +1,15 @@
 
-
-# Create your views here.
 # POSTS
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.permissions import IsAuthenticated
 from posts.models import Post
 from posts.serializers import PostSerializer
 from django.core.exceptions import ValidationError
+from .permissions import AdminHasEditPermission, AdminHasReadPermission
 
-class AdminHasEditPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Comprueba cuales son los permisos de edicion del post
-        if obj.edit_permission == Post.PUBLIC:
-            return True
-        elif obj.edit_permission == Post.AUTHENTICATED:
-            return request.user.is_authenticated
-        elif obj.edit_permission == Post.TEAM:
-            return request.user.team == obj.author.team
-        elif obj.edit_permission == Post.AUTHOR:
-            return request.user == obj.author
-        else:
-            return False
-        
-class AdminHasReadPermission(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        # Comprueba cuales son los permisos de lectura del post
-        if obj.read_permission == Post.PUBLIC:
-            return True
-        elif obj.read_permission == Post.AUTHENTICATED:
-            return request.user.is_authenticated
-        elif obj.read_permission == Post.TEAM:
-            return request.user.team == obj.author.team
-        elif obj.read_permission == Post.AUTHOR:
-            return request.user == obj.author
-        else:
-            return False
+
 
 
 # View for create POST
