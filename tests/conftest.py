9435b39b316ed25e6afcb29c1253de9000c2e84a
EstebanMa12 <desteban.maya@udea.edu.co>
2023-12-18 20:19:09 -0500

    Update tests to use pytest fixtures and Django settings.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,24 @@ register(UserFactory)
 register(PostFactory)
 register(LikesFactory)
 register(CommentsFactory)
-@pytest.fixture()
-def user(db, django_user_model):
-    return django_user_model.objects.create_user(
-        username='testuser',
-        password='password',
-        team='team1',
-        is_superuser=False,
-        is_admin=False,
-    )
+
+
+# @pytest.fixture()
+# def user(db, django_user_model):
+#     return django_user_model.objects.create_user(
+#         username='testuser',
+#         password='password',
+#         team='team1',
+#         is_superuser=False,
+#         is_admin=False,
+#     )
+
+import os
+import django
+from django.conf import settings
+
+# Set the DJANGO_SETTINGS_MODULE environment variable.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avanzatech_blog.settings')
+
+# Configure Django settings.
+django.setup()
--- tests/posts/test_models.py
+++ tests/posts/test_models.py
@@ -1,40 +1,11 @@
-from django.test import TestCase
-from ...posts.models import Post
+import pytest
 
-class PostModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        # Crear un objeto Post para usarlo en las pruebas
-        Post.objects.create(
-            title='Título de prueba',
-            content='Contenido de prueba',
-            author_id=1,
-            read_permission=Post.PUBLIC,
-            edit_permission=Post.PUBLIC
-        )
 
-    def test_str_representation(self):
-        post = Post.objects.get(id=1)
-        self.assertEqual(str(post), 'Título de prueba')
+pytestmark = pytest.mark.django._db
 
-    def test_ordering(self):
-        # Crear más objetos Post para probar el ordenamiento
-        Post.objects.create(
-            title='Título 1',
-            content='Contenido 1',
-            author_id=1,
-            read_permission=Post.PUBLIC,
-            edit_permission=Post.PUBLIC
-        )
-        Post.objects.create(
-            title='Título 2',
-            content='Contenido 2',
-            author_id=1,
-            read_permission=Post.PUBLIC,
-            edit_permission=Post.PUBLIC
-        )
-
-        posts = Post.objects.all()
-        self.assertEqual(posts[0].title, 'Título de prueba')
-        self.assertEqual(posts[1].title, 'Título 2')
-        self.assertEqual(posts[2].title, 'Título 1')
+class TestPostModel:
+    def test_str_return(self,post_factory):
+        post = post_factory(title='test-post')
+        assert post._str_() == 'test-post'
+        
+        
